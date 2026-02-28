fb0e8508d4f09988b8ab059d637c5bbb3fe8f992
Andrii Chychkan <mywork1728@gmail.com>
2026-02-28 23:24:07 +0200

    added college erp and menus

---
+++ addons/college_erp/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'College ERP',
+    'version': '1.0',
+    'summary': 'An ERP college education',
+    'author': 'Andrii Chychkan',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'category': 'Education',
+    'sequence': 1,
+    'data': [
+        'views/college_erp_menus.xml',
+    ],
+}
